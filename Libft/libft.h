4578b81e9866855cab842a289aba1f634f418d4c
Namwoo Kim <namwkim@c4r8s8.42seoul.kr>
2021-05-05 19:41:40 +0900

    123

--- Libft/libft.h
+++ Libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: namwkim <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/05 15:24:09 by namwkim           #+#    #+#             */
-/*   Updated: 2021/05/05 18:21:25 by namwkim          ###   ########.fr       */
+/*   Updated: 2021/05/05 19:41:29 by namwkim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,12 +23,14 @@ int				ft_isalpha(int c);
 int				ft_isascii(int c);
 int				ft_isdigit(int c);
 int				ft_isprint(int c);
-void			*ft_memccpy(void *dst, void *src, int c, unsigned int n);
 void			*ft_memchr(const void *s, int c, unsigned int n);
 void			*ft_memcpy(void	 *dst, void *src, unsigned int n);
+void			*ft_memccpy(void *dst, void *src, int c, unsigned int n);
 void			*ft_memmove(void *dest, const void *src, unsigned int n);
 void			*ft_memset(void *s, int c, unsigned int n);
+int				ft_memcmp(const void *s1, const void *s2);
 char			*ft_strchr(const char *s, int c);
+char			*ft_strrchr(const char *s, int c);
 unsigned int	ft_strlcat(char *dst, const char *src, unsigned int dstsize);
 unsigned int	ft_strlcpy(char *dst, char *src, unsigned int size);
 unsigned int	ft_strlen(const char *s);
@@ -36,8 +38,10 @@ int				ft_strncmp(const char *s1, const char *s2, unsigned int n);
 char			*ft_strnstr(char *haystack, char *needle, unsigned int len);
 int				ft_tolower(int c);
 int				ft_toupper(int c);
+/* use malloc */
 void			*ft_calloc(unsigned int count, unsigned int size);
 char			*ft_strdup(const char *s1);
+
 
 /* Part2 - Additional functions */
 char			*ft_substr(char const *s, unsigned int start, size_t len);
