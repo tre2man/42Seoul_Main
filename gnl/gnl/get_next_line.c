122b566e1b7ce358eead5f6f7f1cb5fe2118d033
tre2man <kimtree3940@gmail.com>
2021-05-18 19:01:56 +0900

    123

--- gnl/gnl/get_next_line.c
+++ gnl/gnl/get_next_line.c
@@ -6,14 +6,45 @@
 /*   By: namwkim <namwkim@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/17 14:19:39 by namwkim           #+#    #+#             */
-/*   Updated: 2021/05/18 14:40:45 by namwkim          ###   ########.fr       */
+/*   Updated: 2021/05/18 18:53:58 by namwkim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include <stdio.h>
 
 int			get_next_line(int fd, char **line)
 {
-	*line = ft_beforestr(*line);
-	return ((int)read(fd, line, BUFFER_SIZE));
+	char	*str;
+	
+	str = (char*)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	fd = read(fd, str, BUFFER_SIZE);
+	*line = str;
+	return (fd);
+}
+
+int main()
+{
+	int             fd;
+	int             i;
+	int             j;
+	char    		*line = 0;
+	char			*lineadress[66];
+	
+	j = 1;
+	
+	if (!(fd = open("alphabet", O_RDONLY)))
+	{
+		printf("\nError in open\n");
+		return (0);
+	}
+	while ((i = get_next_line(fd, &line)) > 0)
+	{
+		printf("|%s\n", line);
+		lineadress[j - 1] = line;
+		j++;
+	}
+	printf("|%s\n", line);
+	free(line);
+	close(fd);
 }
--- gnl/gnl/get_next_line.h
+++ gnl/gnl/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: namwkim <namwkim@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/17 14:20:21 by namwkim           #+#    #+#             */
-/*   Updated: 2021/05/18 14:36:53 by namwkim          ###   ########.fr       */
+/*   Updated: 2021/05/18 18:56:16 by namwkim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,11 @@
 # define GET_NEXT_LINE_H
 # include <unistd.h>
 # include <stdlib.h>
-# include <stdio.h>
 # include <fcntl.h>
+
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 32
+#endif
 
 /*
 ** get_next_line.c
@@ -25,10 +28,6 @@ int			get_next_line(int fd, char **line);
 /*
 ** get_next_line_utils.c
 */
-char		*ft_beforestr(char *str);
-char		*ft_strjoin(char *s1, char *s2);
-size_t		ft_newstrlen(char *s);
-size_t		ft_strlen(char *str);
 
 #endif
 
